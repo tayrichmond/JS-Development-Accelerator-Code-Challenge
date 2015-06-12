d57461ef77480165185dc51f1eb6c66ef2510b64
Taylor Richmond <taylorichmond@gmail.com>
2015-06-11 17:18:22 -0700

    Add comments. Create universal port

--- node_js_server/server.js
+++ node_js_server/server.js
@@ -1,17 +1,24 @@
+//Require http and fs modules.
 var http = require("http");
 var fs = require("fs");
-var port = 3000;
+//Create port.
+var port = process.env.PORT || 3000;
 
+//Read from html file.
 fs.readFile("./index.html", function(error, html) {
+  //Add error checking.
   if(error) {
     throw error;
   }
 
+  //Create server.
   http.createServer(function(request, response) {
     response.writeHeader(200, {"Content-Type": "text/html"});
     response.write(html);
     response.end();
+    //Listen on port.
   }).listen(port);
 });
 
+//Listening message on console.
 console.log("Server is listening at port " + port);
